a48f17a327c455d97f203a6b5b801bc7aa8c23d2
shubhamrustagi <rustagishubham2000@gmail.com>
2024-10-21 21:19:02 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,25 @@
+function extractDate() {
+    const inputDate = document.getElementById('dateInput').value;
+    const minDate = new Date('2014-08-21');
+    const maxDate = new Date();
+    const date = new Date(inputDate);
+    if (date < minDate) {
+        document.getElementById('linker').innerHTML ="The date cannot be before 21 August 2014.";
+        return;
+    }
+    if (date > maxDate) {
+        document.getElementById('linker').innerHTML = "The date cannot be in the future.";
+        return;
+    }
+    if (inputDate) {
+        const dateParts = inputDate.split('-');
+        const day = date.getDate();
+        const month = date.toLocaleString('default', { month: 'long' });
+        const year = date.getFullYear();
+        const linkWithDate = `https://www.nytimes.com/crosswords/game/mini/${dateParts[0]}/${dateParts[1]}/${dateParts[2]}`
+        
+        document.getElementById('linker').innerHTML = `<a href="${linkWithDate}">${day +" "+ month + " " + year}</a>`
+    } else {
+        document.getElementById('linker').innerHTML = "Please select a date."
+    }
+}
